3bf8684507a881a0183abda851d81d768deb7f89
Potter Dai <potterdaica@gmail.com>
2017-04-29 00:51:09 -0400

    Extra funds.

--- lib/string_ext.ex
+++ lib/string_ext.ex
@@ -2,7 +2,16 @@ defmodule StringExt do
   @moduledoc """
   Documentation for StringExt.
   """
-  @spec is_nil_or_empty(binary) :: integer
+  @spec is_nil_or_empty(binary) :: boolean
   def is_nil_or_empty(val), do:
     is_nil(val) || String.length(val) == 0
+
+  @spec reach_length(binary, integer) :: boolean
+  def reach_length(val, length), do:
+    !is_nil(val) && String.length(val) >= length
+
+  @spec within_length(binary, integer, integer) :: boolean
+  def within_length(val, min, max), do:
+    !is_nil(val) && String.length(val) >= min && String.length(val) <= max
+
 end
